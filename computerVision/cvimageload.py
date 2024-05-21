bfbc6d80b3975baa83bea56afdad88e16d860134
mxwlc <maxwellcorey85@gmail.com>
2024-05-21 19:43:55 +0100

    opencv loading image and videos done

---
+++ computerVision/cvimageload.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import sys
+import time
+
+img = cv.imread(cv.samples.findFile("images/cleo0304.jpg"))
+
+if img is None:
+    sys.exit("Could not find image")
+
+cv.imshow("Display Image", img)
+k = cv.waitKey(0)
+
+if k == ord('s'):
+    cv.imwrite("images/output.jpg", img)

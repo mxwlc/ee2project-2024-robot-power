a87ebe5c8fcf9fcd7ee3c44b11925a9253459f1c
mxwlc <maxwellcorey85@gmail.com>
2024-05-28 18:04:06 +0100

    Header files

---
+++ computer_vision_cpp/src/marker_dict.cpp
@@ -0,0 +1,53 @@
+#include "../include/marker_dict.hpp"
+
+marker_dict::marker_dict()
+{
+    std::cout << "Marker Dictionary Initialisation Start" << std::endl;
+    std::map<int, states> temp_map({{0, states::STOP}});
+    marker_map = temp_map;
+    std::cout << "Marker Dictionary Initialisation End" << std::endl;
+}
+
+marker_dict::marker_dict(std::map<int, states> &dict)
+{
+    marker_map = dict;
+}
+
+marker_dict::~marker_dict()
+{
+    std::cout << "Marker Dictionary Leaves Scope" << std::endl;
+}
+
+void marker_dict::add_marker(int id, states marker_state)
+{
+    marker_map.insert({id, marker_state});
+}
+
+states marker_dict::marker_translate(int id)
+{
+    return marker_map[id];
+}
+
+std::map<int, states> marker_dict::return_dict()
+{
+    return marker_map;
+}
+
+void marker_dict::print_dict()
+{
+    std::cout << "Key, Value " << std::endl;
+    for (auto &t : marker_map)
+    {
+        std::cout << t.first << " " << states(t.second) << std::endl;
+    }
+}
+
+// void marker_dict::save_dict()
+// {
+//     std::ofstream ofs("marker_dict");
+
+//     {
+//         boost::archive::text_oarchive oa(ofs);
+//         oa << marker_map;
+//     }
+// }
